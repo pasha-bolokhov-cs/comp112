4c00ac4596a472751e6e50f85b3b6fc5ed6f8dd4
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-10 13:36:54 -0700

    Check the hostname in 'cst-grant.sh'
    'cst-grant.sh' now checks the current hostname before
    changing the permissions, and will not run unless it is
    executed in CST

--- cst-grant.sh
+++ cst-grant.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
 
-setfacl -R -m u:rthorndy:rwx handin
+csthost=deepblue.cs.camosun.bc.ca
+
+if [ "x$HOSTNAME" = "x$csthost" ]; then
+	setfacl -R -m u:rthorndy:rwx handin
+else
+	echo "This script should only be run on $csthost" 1>&2
+	exit 1
+fi
 
