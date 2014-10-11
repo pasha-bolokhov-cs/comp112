afdfc644c384a02a51582e9be2ce7d85bc368d20
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-10 23:09:59 -0700

    Optimize cst-grant.sh
    Optimize cst-grant.sh by organizing the variables and adding
    a comment

--- cst-grant.sh
+++ cst-grant.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env sh
+#
+#  Add permissions for the instructor to be able to chdir()
+#  into 'handin' and also grant access to the contents therein
+#
 
 csthost=deepblue.cs.camosun.bc.ca
+cstinstructor=rthorndy
 
 if [ "x$HOSTNAME" = "x$csthost" ]; then
-	setfacl    -m u:rthorndy:x   ~
-	setfacl    -m u:rthorndy:x   .
-	setfacl -R -m u:rthorndy:rwx handin
+	setfacl    -m u:${cstinstructor}:x   ~
+	setfacl    -m u:${cstinstructor}:x   .
+	setfacl -R -m u:${cstinstructor}:rwx handin
 else
 	echo "This script should only be run on $csthost" 1>&2
 	exit 1
