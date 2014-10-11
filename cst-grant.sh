7d492ce1161817edbae6835c6eb53c0a4916852b
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-10 23:01:40 -0700

    Add permission to current directory and home
    Add permission to access (x) the current directory
    and the home directory

--- cst-grant.sh
+++ cst-grant.sh
@@ -3,6 +3,8 @@
 csthost=deepblue.cs.camosun.bc.ca
 
 if [ "x$HOSTNAME" = "x$csthost" ]; then
+	setfacl    -m u:rthorndy:x   ~
+	setfacl    -m u:rthorndy:x   .
 	setfacl -R -m u:rthorndy:rwx handin
 else
 	echo "This script should only be run on $csthost" 1>&2
