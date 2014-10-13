a022f91e85f7769aefcccc32b2f1b231313d64c1
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-12 21:03:18 -0700

    Correct 'cst-grant.sh' and add check for 'core.filemode'

--- cst-grant.sh
+++ cst-grant.sh
@@ -7,15 +7,22 @@
 csthost=deepblue.cs.camosun.bc.ca
 cstinstructor=rthorndy
 
-# Git changes mode to 674 for some reason
-chmod -R 755 .
 
-if [ "x$HOSTNAME" = "x$csthost" ]; then
-	setfacl    -m u:${cstinstructor}:x   ~
-	setfacl    -m u:${cstinstructor}:x   .
-	setfacl -R -m u:${cstinstructor}:rwx handin
-else
+if [ "x$HOSTNAME" != "x$csthost" ]; then
 	echo "This script should only be run on $csthost" 1>&2
 	exit 1
 fi
+
+# 'core.filemode' should be set to false
+if [ "x$(git config core.filemode)" != "xfalse" ];
+	echo "Set 'core.filemode' to false to make Git ignore permission changes" 1>&2
+	exit 1
+fi
+
+# Git changes mode to 674 for some reason
+chmod -R 755 .
+
+setfacl    -m u:${cstinstructor}:x   ~
+setfacl    -m u:${cstinstructor}:x   .
+setfacl -R -m u:${cstinstructor}:rwx handin
 
