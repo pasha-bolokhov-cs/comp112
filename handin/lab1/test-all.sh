2092a822d59823bcd5a6874476079ef4071719bf
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-12 20:14:19 -0700

    Upgrade 'test-all.sh' to do a series of individual tests

--- handin/lab1/test-all.sh
+++ handin/lab1/test-all.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
->out.actual
-
 for q in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17; do 
-	for k in in${q}_*; do
-		./lab1q${q}.py < $k >> out.actual
-	done
+    for k in in${q}_*; do
+	out=out${q}_${k##*_}.actual
+	./lab1q${q}.py < $k > $out
+	diff ${out%%.actual}.expect $out >/dev/null ||
+	{ echo "'diff ${out%%.actual}.expect $out' failed" 1>&2; exit 1; }
+    done
 done
-diff out.expect out.actual
+
 
 
