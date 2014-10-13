86f2869f1d095abc5bd85ebfd18402a4ac10cee4
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-12 17:44:36 -0700

    Finish main work on 'lab2.py'
    Main work on this file is finished and testing can begin

--- handin/lab2/lab2.py
+++ handin/lab2/lab2.py
@@ -122,9 +122,7 @@ def Q13(c):
     """
     Question 13
     """
-    u = c / 1.12
-    
-    return "%.2f" % u
+    return c / 1.12
 
 
 
@@ -135,7 +133,7 @@ def Q14(h, w):
     """
     index = w / ( h * h )
     
-    return "%.1f"   %   index
+    return index
 
 
 
@@ -153,7 +151,7 @@ def Q16(sg, eg, days):
     """
     Question 16
     """
-    return "%.2f" % (  ((eg - sg) * 0.01433  +  days * 0.0902) * 1.05  )
+    return ((eg - sg) * 0.01433  +  days * 0.0902) * 1.05
 
 
 
@@ -164,7 +162,7 @@ def Q17(P, R, t):
     """
     R = R / 12.
     T = float(t) * 12.
-    return "%.2f"  %  ( ( P * R * math.pow(1 + R, T) ) / ( math.pow(1 + R, T) - 1 ) )
+    return ( P * R * math.pow(1 + R, T) ) / ( math.pow(1 + R, T) - 1 )
 
 
 
@@ -177,23 +175,23 @@ def main():
     # map of functions, number and types of their arguments,
     # and output formats for its return values
     #
-    targs = {"q1":  [Q1,  [float]],                      ["%f"],
-             "q2":  [Q2,  [float]],                      ["%f"],
-             "q3":  [Q3,  [float]],                      ["%f"],
-             "q4":  [Q4,  [float, float]],               ["%f"],
-             "q5":  [Q5,  [float, float]],               ["%f"],
-             "q6":  [Q6,  [float, float]],               ["%f"],
-             "q7":  [Q7,  [float]],                      ["%f"],
-             "q8":  [Q8,  [float]],                      ["%f"],
-             "q9":  [Q9,  [int, int]],                   ["%f"],
-             "q10": [Q10, [float, float, float]],        ["%f"],
-             "q11": [Q11, [int, int]],                   ["%d", "%d", "%d"],
-             "q12": [Q12, [float, float, float, float]], ["%f"],
-             "q13": [Q13, [float]],                      ["%.2f"],
-             "q14": [Q14, [float, float]],               ["%.1f"],
-             "q15": [Q15, [float]],                      ["%f"],
-             "q16": [Q16, [float, float, int]],          ["%.2f"],
-             "q17": [Q17, [int, float, int]],            ["%.2f"]}
+    targs = {"q1":  [Q1,  [float],                      ["!"]],
+             "q2":  [Q2,  [float],                      ["!"]],
+             "q3":  [Q3,  [float],                      ["!"]],
+             "q4":  [Q4,  [float, float],               ["!"]],
+             "q5":  [Q5,  [float, float],               ["!"]],
+             "q6":  [Q6,  [float, float],               ["!"]],
+             "q7":  [Q7,  [float],                      ["!"]],
+             "q8":  [Q8,  [float],                      ["!"]],
+             "q9":  [Q9,  [int, int],                   ["!"]],
+             "q10": [Q10, [float, float, float],        ["!"]],
+             "q11": [Q11, [int, int],                   ["!", "!", "!"]],
+             "q12": [Q12, [float, float, float, float], ["!"]],
+             "q13": [Q13, [float],                      ["%.2f"]],
+             "q14": [Q14, [float, float],               ["%.1f"]],
+             "q15": [Q15, [float],                      ["!"]],
+             "q16": [Q16, [float, float, int],          ["%.2f"]],
+             "q17": [Q17, [int, float, int],            ["%.2f"]]}
 
     # get a function name from 'argv'
     if len(sys.argv) - 1 == 0:
@@ -205,6 +203,32 @@ def main():
         sys.stderr.write("Unrecognized function: `%s'\n" % request)
         exit(1)
 
+    # find the function and its arguments
+    quest = targs[request][0]
+    types = targs[request][1]
+    nargs = len(types)
+    formats = targs[request][2]
+    nres = len(formats)
+    args = []
+
+    # get the input and convert if necessary
+    for a in types:
+        value = input()
+        args.append(a(value))
+    
+    # call the actual function
+    returns = quest(*args)
+    
+    # make a list if this was just one return value
+    if type(returns) != type([]):
+        results = [returns]
+
+    # print out all results in the correct format
+    for r in xrange(len(formats)):
+        if formats[r] == "!":
+            print results[r]
+        else:
+            print formats[r] % results[r]
 
 
 #
