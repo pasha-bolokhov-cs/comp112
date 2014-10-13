ae869f6b0c7f5568c2d02f7f2f97d012f2c750f2
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-10-12 18:02:33 -0700

    Add  and run 'test-all.sh' script

---
+++ handin/lab2/test-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+>out.actual
+
+for q in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17; do 
+	for k in in${q}_*; do
+		./lab2.py q${q} < $k >> out.actual
+	done
+done
+diff out.expect out.actual
